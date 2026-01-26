060dc45b50139b7526ab5a0ea852b0fe5cc22d38
fredypdp <fredrodrigues795@gmail.com>
2026-01-26 01:44:22 +0100

    correção no UserInfoCard

--- src/components/user-profile/UserInfoCard.tsx
+++ src/components/user-profile/UserInfoCard.tsx
@@ -142,7 +142,7 @@ export default function UserInfoCard() {
                         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                       </svg>
                     ) : (
-                      <button onClick={() => null} disabled={null} className="inline-flex items-center justify-center font-medium gap-2 rounded-lg transition bg-orange-500 text-white shadow-theme-xs hover:bg-orange-600 disabled:bg-orange-300 px-2 py-1.5 text-sm">Verificar e-mail</button>
+                      <button onClick={() => null} disabled={false} className="inline-flex items-center justify-center font-medium gap-2 rounded-lg transition bg-orange-500 text-white shadow-theme-xs hover:bg-orange-600 disabled:bg-orange-300 px-2 py-1.5 text-sm">Verificar e-mail</button>
                     )
                   )}
                 </div>
