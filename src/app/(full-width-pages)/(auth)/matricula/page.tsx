e951ba7fc81e855b3b196caba0f786118511cedb
Fredy <fredrodrigues795@gmail.com>
2026-07-19 17:35:05 +0100

    Update page.tsx

--- src/app/(full-width-pages)/(auth)/matricula/page.tsx
+++ src/app/(full-width-pages)/(auth)/matricula/page.tsx
@@ -1,6 +1,6 @@
 import MatriculaPublicPage from "./MatriculaPublicPage";
 import { Metadata } from "next";
 
-export const metadata: Metadata = { title: "Fazer matrícula", description: "Solicite a sua matrícula numa academia" };
+export const metadata: Metadata = { title: "Fazer matrícula", description: "Solicite a sua matrícula numa instituição" };
 
 export default function Page() { return <MatriculaPublicPage />; }
